0c3b5e691d1b1823be8a0a82521ded8f8373b99e
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-08-31 19:38:20 +0300

    Create the GameBoard component and update the gameBoard state immutably

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   return (
@@ -6,9 +7,9 @@ function App() {
       <div id="game-container">
         <ol id="players">
           <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="0"/>
+          <Player initialName="Player 2" symbol="0" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   )
